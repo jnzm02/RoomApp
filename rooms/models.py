d6a7a48adfc03357346f914b3c4802f021e940d5
Nizami Jussupov <jussupov.02@gmail.com>
2022-06-14 10:00:51 +0600

    added new field for Room model: password

--- rooms/models.py
+++ rooms/models.py
@@ -10,12 +10,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Room(models.Model):
     title = models.CharField(max_length=32)
     date = models.DateField(auto_now_add=True)
-    creator = models.ForeignKey(
-        get_user_model(), on_delete=models.CASCADE
-    )
+    creator = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     is_private = models.BooleanField(default=False)
-
-
+    password = models.CharField(max_length=32, blank=True)
 
     # is_private = models.BooleanField(default=False)
 
